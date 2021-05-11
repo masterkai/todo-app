87b31ae3f2dce9bb66cb80eabf6d7ccb0d202cb7
max <max@verygps.com.tw>
2021-05-11 17:58:28 +0800

    add list item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss'
 import TabButton from "./components/TabButton";
 import {useState} from "react";
+import ListItem from "./components/ListItem";
 
 function App() {
   const [tabButtons, setTabButtons] = useState([
@@ -8,33 +9,61 @@ function App() {
     {name: 'Active', active: false},
     {name: 'Completed', active: false},
   ])
+  const [lists, setLists] = useState([])
+  const [value, setValue] = useState('')
   const handleTabsClick = (index) => {
     const newTabButtons = [...tabButtons]
     newTabButtons.forEach(tab => tab.active = false)
     newTabButtons[index]["active"] = true;
     setTabButtons(newTabButtons)
   }
+  const handleChange = ({target: {value}}) => {
+    setValue(value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(value){
+      const stamp = new Date().toString()//?
+      const newLists = [...lists,{id:stamp,value,checked: false}]
+      setLists(newLists)
+      setValue('')
+    } else {
+      alert('Hi there! type something!!pls~')
+    }
+  }
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">What needs to be
-            done?</label></h2>
+            done?</label>
+        </h2>
         <input
           type="text"
           id="new-todo-input"
           className="input input__lg"
           name="text"
           autoComplete="off"
-          value=""/>
-        <button type="submit" className="btn btn__primary btn__lg">Add</button>
+          onChange={handleChange}
+          value={value}/>
+        <button
+          type="submit"
+          className="btn btn__primary btn__lg">Add
+        </button>
       </form>
       <div className="filters btn-group stack-exception">
-        {tabButtons.map((b,i) => <TabButton key={b.name} index={i} name={b.name} active={b.active} handleTabsClick={handleTabsClick}/>)}
+        {tabButtons.map((b, i) => (
+          <TabButton
+            key={b.name}
+            index={i}
+            name={b.name}
+            active={b.active}
+            handleTabsClick={handleTabsClick}/>
+        ))}
       </div>
       <h2 id="list-heading" tabIndex="-1">0 tasks remaining</h2>
       <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
-
+        {lists && lists.map(item => <ListItem key={item.id} value={item.value} checked={item.checked}/>)}
       </ul>
     </div>
   )
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const ListItem = () => {
+const ListItem = ({id, value, checked}) => {
   return (
     <div className="stack-small">
       <div className="c-cb">
-        <input id="todo-5pvnScnsraaazWvhOSuS8" type="checkbox"/>
+        <input id={`todo-${id}`} type="checkbox" checked={checked}/>
         <label
           className="todo-label"
-          htmlFor="todo-5pvnScnsraaazWvhOSuS8">milk</label>
+          htmlFor={`todo-${id}`}>{value}</label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn">Edit <span className="visually-hidden">milk</span></button>
-        <button type="button" className="btn btn__danger">Delete <span className="visually-hidden">milk</span>
+        <button type="button" className="btn">Edit <span className="visually-hidden">{value}</span></button>
+        <button type="button" className="btn btn__danger">Delete <span className="visually-hidden">{value}</span>
         </button>
       </div>
     </div>
